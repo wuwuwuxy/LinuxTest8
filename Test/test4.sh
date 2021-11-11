8807c78639cd11017f7e70986a4c329e9a2d50b9
wuwuwu <2465489619@qq.com>
2021-11-11 22:18:22 +0800

    tests

---
+++ Test/test4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read name animal1 animal2 animal3
+name=pengyuan;
+animal1=panda;
+animal2=lion;
+animal3=duck;
+echo "welcome to our ${name}_zoo"
+echo "we have $animal1,$animal2,$animal3"
